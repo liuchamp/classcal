c9623abd1f6b9a2121e5028fcaeb5b2de0e4ff81
Champ Liu <2756154878@qq.com>
2018-04-16 21:20:24 +0800

    初始化数据结构项目

---
+++ datastruct/cmake-build-debug/CMakeFiles/datastruct.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datastruct.dir/main.cpp.o"
+  "datastruct.pdb"
+  "datastruct"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datastruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
