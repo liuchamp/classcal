018d0468e4a8c24fdd98cf64348af0b2ab2aaf5f
yexingyun <yexingyun9210@gmail.com>
2019-08-26 21:59:39 +0700

    重新编写代码，重构系统

--- datastruct/main.cpp
+++ datastruct/main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
-#include <gtest/gtest.h>
 
-int Add(int a, int b) {
-    return a + b;
-}
-
-TEST(testCase, test0) {
-    EXPECT_EQ(14, Add(4, 10));
-}
-
-TEST(testCase, test1) {
-    ASSERT_EQ(12, Add(5, 7));
-}
-
-TEST(testCase, test2) {
-    EXPECT_EQ(18, Add(8, 10)) << "bugs";
-}
-
-int main(int argc, char **argv) {
+int main() {
     std::cout << "Hello, World!" << std::endl;
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();  // 执行所有的 test case
+    return 0;
 }
