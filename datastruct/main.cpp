b21a582a2fdd800d439bcfc72e7e343c8b0115e3
yexingyun <yexingyun9210@gmail.com>
2019-08-26 11:38:24 +0700

    add gtest for unit test

--- datastruct/main.cpp
+++ datastruct/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <gtest/gtest.h>
 
-int main() {
+int Add(int a, int b) {
+    return a+b;
+}
+
+TEST(testCase, test0){
+EXPECT_EQ(14, Add(4, 10));
+}
+
+TEST(testCase, test1){
+ASSERT_EQ(6, Add(5, 7));
+}
+
+TEST(testCase, test2){
+EXPECT_EQ(28, Add(8, 10)) << "bugs";
+}
+
+int main(int argc, char ** argv) {
     std::cout << "Hello, World!" << std::endl;
-    return 0;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();  // 执行所有的 test case
 }
