e2a592ea0532a1ecbb3b9fea75fcbc659474b7e4
yexingyun <yexingyun9210@gmail.com>
2019-08-26 14:29:13 +0700

    start all test

--- datastruct/main.cpp
+++ datastruct/main.cpp
@@ -2,22 +2,22 @@
 #include <gtest/gtest.h>
 
 int Add(int a, int b) {
-    return a+b;
+    return a + b;
 }
 
-TEST(testCase, test0){
-EXPECT_EQ(14, Add(4, 10));
+TEST(testCase, test0) {
+    EXPECT_EQ(14, Add(4, 10));
 }
 
-TEST(testCase, test1){
-ASSERT_EQ(6, Add(5, 7));
+TEST(testCase, test1) {
+    ASSERT_EQ(12, Add(5, 7));
 }
 
-TEST(testCase, test2){
-EXPECT_EQ(28, Add(8, 10)) << "bugs";
+TEST(testCase, test2) {
+    EXPECT_EQ(18, Add(8, 10)) << "bugs";
 }
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv) {
     std::cout << "Hello, World!" << std::endl;
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();  // 执行所有的 test case
